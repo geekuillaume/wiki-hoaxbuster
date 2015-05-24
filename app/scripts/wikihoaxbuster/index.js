33acd339fb92ee9f1e0ec127c0fb94fbfd0dea21
Geekuillaume <geek.uillaume@gmail.com>
2015-05-24 18:00:18 +0200

    Added token score computing

--- app/scripts/wikihoaxbuster/index.js
+++ app/scripts/wikihoaxbuster/index.js
@@ -5,7 +5,7 @@ var _ = require('lodash');
 
 var whoColor = require('./whocolor.js');
 var getUsersInfos = require('./get-users-info.js');
-
+var tokenScore = require('./tokenScore.js');
 
 function hoaxBuster() {
   chrome.runtime.sendMessage({
@@ -13,13 +13,21 @@ function hoaxBuster() {
     spinner: true
   });
   whoColor.get(function(err, results) {
-    $('#mw-content-text').html(results.html);
     getUsersInfos(_.chain(results.authors).filter('anon', false).map('name').value(), function(err, authors) {
-      console.log('got:', authors);
-    });
-    chrome.runtime.sendMessage({
-      type: 'wikihoaxbuster::setSpinner',
-      spinner: false
+      console.log('got:', authors, results);
+
+      var tokens = tokenScore.compute(results.tokens, {
+        revisions: results.revisions,
+        authors: authors
+      });
+
+      console.log(tokens);
+
+      $('#mw-content-text').html(results.html);
+      chrome.runtime.sendMessage({
+        type: 'wikihoaxbuster::setSpinner',
+        spinner: false
+      });
     });
   });
 }
---
+++ app/scripts/wikihoaxbuster/tokenScore.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var _ = require('lodash');
+
+function getRevisionAgeScore(token, infos) {
+
+  var age = _.keys(infos.revisions).sort(function(a, b) {
+    return a < b;
+  }).indexOf(token.revid);
+
+  if (age === 0)
+    return 0;
+  if (age < 5)
+    return 0.3;
+  if (age < 15)
+    return 0.7;
+  return 1;
+}
+
+function monthOld(d1) {
+    var months;
+    var d2 = new Date();
+    d1 = new Date(d1);
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth() + 1;
+    months += d2.getMonth();
+    return months <= 0 ? 0 : months;
+}
+
+function authorScore(token, infos) {
+  var author = infos.authors[token.authorid];
+
+  if (!author)
+    return 0;
+
+  return 0.2 + _.min([0.4, author.editCount / 10000]) + _.min([0.4, monthOld(author.registration) / 24]);
+}
+
+exports.compute = function(tokens, infos) {
+  return _.each(tokens, function(token) {
+    token.ageScore = getRevisionAgeScore(token, infos);
+    token.authorScore = authorScore(token, infos);
+  });
+};
