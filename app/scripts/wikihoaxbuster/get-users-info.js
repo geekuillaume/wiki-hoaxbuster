f3dfd3d82a53d1bc97e37a7f6dea04bede1a6b7a
buathi_q <buathi_q@epitech.eu>
2015-05-24 14:16:29 +0200

    Add getUsersInfo

--- app/scripts/wikihoaxbuster/get-users-info.js
+++ app/scripts/wikihoaxbuster/get-users-info.js
@@ -1,8 +1,41 @@
 'use strict';
 
+var $ = require('wlt-zepto');
 var _ = require('lodash');
 var async = require('async');
 
-module.exports = function getUsersInfo(users) {
+module.exports = function getUsersInfo(usernames, cb) {
+  var users = {};
 
+  async.eachLimit(_.chunk(usernames, 50), 5, function(usernames, cb) {
+    $.ajax({
+      url: 'https://en.wikipedia.org/w/api.php',
+      data: {
+        action: 'query',
+        list: 'users',
+        format: 'json',
+        usprop: 'editcount|registration',
+        ususers: usernames.join('|')
+      },
+      dataType: 'json',
+      success: function(data) {
+        data.query.users.forEach(function(user) {
+          if(user.userid) {
+            users[user.userid] = user;
+          }
+        });
+
+        return cb(null);
+      },
+      error: function(err) {
+        return cb(err);
+      }
+    });
+  }, function(err) {
+    if(err) {
+      return cb(err);
+    }
+
+    return cb(null, users);
+  });
 };
