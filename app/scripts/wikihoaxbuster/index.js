58ab1d0630599cbc7696d18bd608bea978dcc908
buathi_q <buathi_q@epitech.eu>
2015-05-25 12:11:02 +0200

    Add a cache on local storage

--- app/scripts/wikihoaxbuster/index.js
+++ app/scripts/wikihoaxbuster/index.js
@@ -10,12 +10,63 @@ var whoColor = require('./whocolor.js');
 var getUsersInfos = require('./get-users-info.js');
 var tokenScore = require('./tokenScore.js');
 
+function applyHtml(results, authors) {
+  $('#mw-content-text').html(results.html);
+
+  _.each(results.tokens, function(token) {
+    $('.author-tokenid-' + token.index).tooltipster({
+      content: _.template(
+        [
+          '<div class="wikihoaxbuster-tooltip"><span>Score:</span> ${token.score}</div>',
+          '<div class="wikihoaxbuster-tooltip"><span>Author:</span> <a href="https://en.wikipedia.org/wiki/User:${author.name}">${author.name}</a></div>',
+          '<% if(revision.comment) {%><div class="wikihoaxbuster-tooltip"><span>Comment:</span> ${revision.comment}</div><%}%>',
+          '<div class="wikihoaxbuster-tooltip"><span>Date:</span> ${momentFromNow}</div>',
+          '<div class="wikihoaxbuster-tooltip"><a href="https://en.wikipedia.org/w/index.php?diff=${token.revid}">View edit</a></div>'
+        ].join('\n'))({
+          token: token,
+          author: authors[token.authorid] || results.authors[token.authorid] || {name: 'Unknown'},
+          revision: results.revisions[token.revid],
+          momentFromNow: moment(results.revisions[token.revid].timestamp).fromNow()
+        }),
+        contentAsHTML: true,
+        interactive: true,
+        // autoClose: false
+    });
+  });
+
+  chrome.runtime.sendMessage({
+    type: 'wikihoaxbuster::setSpinner',
+    spinner: false
+  });
+}
 
 function hoaxBuster() {
   chrome.runtime.sendMessage({
     type: 'wikihoaxbuster::setSpinner',
     spinner: true
   });
+
+  var name = 'wiki-hoaxbuster-' + $('h1#firstHeading').text();
+  if(localStorage && localStorage.getItem) {
+    var data = localStorage.getItem(name);
+    if(data) {
+      try {
+        data = JSON.parse(data);
+      }
+      catch(e) {
+        data = null;
+        localStorage.removeItem(name);
+      }
+
+      if(data && data.ttl < +new Date()) {
+        localStorage.removeItem(name);
+      }
+      else if(data) {
+        return applyHtml(data.results, data.authors);
+      }
+    }
+  }
+
   whoColor.get(function(err, results) {
     if(err) {
       chrome.runtime.sendMessage({
@@ -36,38 +87,20 @@ function hoaxBuster() {
         return console.warn(err);
       }
 
-      var tokens = tokenScore.compute(results.tokens, {
+      tokenScore.compute(results.tokens, {
         revisions: results.revisions,
         authors: authors
       });
 
-      $('#mw-content-text').html(results.html);
+      if(localStorage && localStorage.setItem) {
+        localStorage.setItem(name, JSON.stringify({
+          results: results,
+          authors: authors,
+          ttl: Date.now() + 1000 * 60 * 60 * 24
+        }));
+      }
 
-      _.each(tokens, function(token) {
-        $('.author-tokenid-' + token.index).tooltipster({
-          content: _.template(
-            [
-              '<div class="wikihoaxbuster-tooltip"><span>Score:</span> ${token.score}</div>',
-              '<div class="wikihoaxbuster-tooltip"><span>Author:</span> <a href="https://en.wikipedia.org/wiki/User:${author.name}">${author.name}</a></div>',
-              '<% if(revision.comment) {%><div class="wikihoaxbuster-tooltip"><span>Comment:</span> ${revision.comment}</div><%}%>',
-              '<div class="wikihoaxbuster-tooltip"><span>Date:</span> ${momentFromNow}</div>',
-              '<div class="wikihoaxbuster-tooltip"><a href="https://en.wikipedia.org/w/index.php?diff=${token.revid}">View edit</a></div>'
-            ].join('\n'))({
-              token: token,
-              author: authors[token.authorid] || results.authors[token.authorid] || {name: 'Unknown'},
-              revision: results.revisions[token.revid],
-              momentFromNow: moment(results.revisions[token.revid].timestamp).fromNow()
-            }),
-            contentAsHTML: true,
-            interactive: true,
-            // autoClose: false
-        });
-      });
-
-      chrome.runtime.sendMessage({
-        type: 'wikihoaxbuster::setSpinner',
-        spinner: false
-      });
+      applyHtml(results, authors);
     });
   });
 }
